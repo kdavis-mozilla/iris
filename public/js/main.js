98f63cba20a9b45add29d0e7bb80fa7deeaee3a9
Kelly Davis <kdavis@mozilla.com>
2015-11-11 18:54:59 +0100

    WIP initial integration of dictate.js

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,6 @@
+/* global Dictate:true*/
 /* global WaveSurfer:true*/
+/* global Transcription:true*/
 /**
  * Main entry point.
  *
@@ -21,13 +23,16 @@
     init: function() {
       document.body.classList.remove('hidden');
 
+      // Initialize dictate.js
+      var dictate = this.initDictate();
+
       // Initialize WaveSurfer
-      this.initWaveSurfer();
+      this.initWaveSurfer(dictate);
     },
 
-    initWaveSurfer: function() {
+    initWaveSurfer: function(dictate) {
       // Create WaveSurfer instance
-      this.wavesurfer = Object.create(WaveSurfer);
+      var wavesurfer = Object.create(WaveSurfer);
 
       // Create WaveSurfer options
       var options = {
@@ -40,16 +45,16 @@
         height: 250
       };
 
+      // Init wavesurfer
+      wavesurfer.init(options);
+
       // Obtain micBtn
       var micBtn = document.querySelector('#micBtn');
-
-      // Init wavesurfer
-      this.wavesurfer.init(options);
 
       // Init Microphone plugin
       var microphone = Object.create(WaveSurfer.Microphone);
       microphone.init({
-        wavesurfer: this.wavesurfer
+        wavesurfer: wavesurfer
       });
       microphone.on('deviceReady', function() {
         console.info('Device ready!');
@@ -62,10 +67,61 @@
       micBtn.onclick = function() {
         if (microphone.active) {
           microphone.stop();
+          dictate.stopListening();
         } else {
           microphone.start();
+          dictate.startListening();
         }
       };
+    },
+
+    initDictate: function() {
+      // Create Transcription to hold spoken text
+      var tt = new Transcription();
+
+      // Obtain textbox
+      var textbox = document.querySelector('#textbox');
+
+      // Create Dictate for speech-to-text
+      var dictate = new Dictate({
+        server: 'ws://bark.phon.ioc.ee:82/english/duplex-speech-api/ws/speech',
+        recorderWorkerPath: 'vendor/dictate.js/lib/recorderWorker.js',
+        onReadyForSpeech: function() {
+          console.info('Ready for speech');
+        },
+        onEndOfSpeech: function() {
+          console.info('End of speech');
+        },
+        onEndOfSession: function() {
+          console.info('End of session');
+        },
+        onPartialResults: function(hypos) {
+          // TODO: Generalize to the case where there are more results
+          tt.add(hypos[0].transcript, false);
+          console.info('onPartialResults: ' + tt.toString());
+          textbox.innerHTML = tt.toString();
+        },
+        onResults: function(hypos) {
+          // TODO: Generalize to the case where there are more results
+          tt.add(hypos[0].transcript, true);
+          console.info('onResults: ' + tt.toString());
+          textbox.innerHTML = tt.toString();
+          tt = new Transcription();
+        },
+        onError: function(code, data) {
+          console.warn('ERR: ' + code + ': ' + (data || ''));
+          dictate.cancel();
+        },
+        onEvent: function(code, data) {
+          console.info(code, data);
+        }
+      });
+
+      // Init Dictate
+      dictate.init();
+
+      // Return dictate
+      return dictate;
     }
   };
 
